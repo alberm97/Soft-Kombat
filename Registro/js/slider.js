846b89cc9f16e5a62fe06d520870fa8b9ba7bee5
Ztatik11 <99404495+Ztatik11@users.noreply.github.com>
2022-06-03 20:55:20 +0200

    Add files via upload

--- Registro/js/slider.js
+++ Registro/js/slider.js
@@ -9,17 +9,25 @@
         let valor;
         const elemento_actual = Number(document.querySelector(".celda_slider--show").dataset.id);
         
-        valor=elemento_actual;
+        valor=elemento_actual-1;
         valor=valor+change;
+
+      //  console.log(change);
         
-        if (valor===0 || valor==sliders.length+1) {
-            valor = valor === 0 ? 1 : sliders.length;
-        }
-        console.log(elemento_actual);
-        console.log(valor);
-        sliders[elemento_actual-1].classList.remove(".celda_slider--show");
-        sliders[valor-1].classList.add(".celda_slider--show");
+       //if (valor<0 || valor>=sliders.length) {
+           // valor = valor < 0 ? 1 : sliders.length-1;
+            valor = valor < 0 ? sliders.length-1 : valor;
+            valor = valor > sliders.length-1 ? 0 :valor;
+
+        //}
+        //console.log("elementom nuevo" + (elemento_actual-1));
+        //console.log("Valor" + valor);
+
+        //console.log(sliders[elemento_actual].className);
+
+        sliders[elemento_actual-1].classList.remove("celda_slider--show");
+        sliders[valor].classList.add("celda_slider--show");
         
     }
-    console.log(sliders);
+    console.log(sliders.length);
 })()
